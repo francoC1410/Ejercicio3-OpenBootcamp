98de4b2b75e8d01900d88a7a4743101185b586bb
francoC1410 <111617652+francoC1410@users.noreply.github.com>
2022-09-05 21:45:09 -0300

    Add files via upload

---
+++ Bucles.java
@@ -0,0 +1,13 @@
+package Tiposdedatos;
+
+public class Bucles {
+
+    public static void main(String[] args) {
+
+        String[] nombres = {"Franco", "Pablo", "Nicolas", "Santiago", "Stefano"};
+
+        for (int i = 0; i < nombres.length; i++) {
+            System.out.println("Mi nombre es: " + nombres[i]);
+        }
+    }
+}
